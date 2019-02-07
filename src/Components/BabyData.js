3337c63b8fc0ba4efab93ba50ce000d7d891500e
Tiago Formosinho Sanchez <tiagosanchez@Martas-iMac.local>
2019-02-07 23:55:33 +0800

    now able to pass data into output

---
+++ src/Components/BabyData.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import BabyFoodOutput from './BabyFoodOutput'
+
+class BabyData extends React.Component {
+
+    render() {
+        const { food } = this.props
+        return (
+            <div>
+                {/* This will happen only if food.data is true */}
+                {food.data && food.data.map(data => {
+                    return <BabyFoodOutput foodData={data} />
+                })}
+            </div>
+        )
+    }
+}
+
+export default BabyData;
