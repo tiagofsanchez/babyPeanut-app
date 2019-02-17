0442cc652ef91074eb9b24e04c1df98ec50dc384
tiagofsanchez <tiagofsanchez@users.noreply.github.com>
2019-02-17 11:42:44 +0800

    working on the output form to see progress

---
+++ src/Components/BabyOutputForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const initialstate = {
+
+}
+
+class BabyOutputForm extends React.Component {
+    
+    state = initialstate;
+    
+    render () {
+        return (
+            <div>
+                I am the Output Form!
+            </div>
+        )
+    }
+}
+
+export default BabyOutputForm;
