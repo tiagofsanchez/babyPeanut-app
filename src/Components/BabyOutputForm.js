b396c98f4c1680fb1d22e0fe0153f743b2ebc1da
tiagofsanchez <tiagofsanchez@users.noreply.github.com>
2019-02-17 12:21:59 +0800

    ouput form... WIP

--- src/Components/BabyOutputForm.js
+++ src/Components/BabyOutputForm.js
@@ -1,19 +1,43 @@
 import React from 'react';
+import { Segment, Form } from 'semantic-ui-react';
 
 
 const initialstate = {
-
+    output:'',
+    datetime: '',
+    text:'',
 }
+
+const outputOptions = [
+    { key: 'motion', text: 'pass motion', value: 'pass motion' },
+    { key: 'urine', text: 'pass urine', value: 'pass urine' },
+    { key: 'both', text: 'urine and motion', value: 'urine and motion' },
+]
 
 class BabyOutputForm extends React.Component {
     
     state = initialstate;
+
+    
     
     render () {
+
+        const { output, datetime, text } = this.state;
+
         return (
-            <div>
-                I am the Output Form!
-            </div>
+            <Segment basic>
+                <Form>
+                <Form.Dropdown
+                            name='output'
+                            onChange={(event, { name, value }) => this.handleChange(event, name, value)}
+                            placeholder='pass urine or motion ...'
+                            selection
+                            options={outputOptions}
+                            value={output}
+                />
+
+                </Form>
+            </Segment>
         )
     }
 }
