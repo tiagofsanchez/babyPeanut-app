781d514514ad059ad543c2eab8dcd3faeb8ab54f
Tiago Formosinho Sanchez <tiagofsanchez@gmail.com>
2019-02-20 18:39:39 +0800

    last version

---
+++ src/Components/BabyFoodEditForm.js
@@ -0,0 +1,17 @@
+import React from 'react'; 
+
+class BabyFoodEditForm extends React.Component {
+
+    render() {
+
+        const { food  } = this.props;
+     
+        
+        return (
+            
+
+        )
+    }
+}
+
+export default BabyFoodEditForm;
