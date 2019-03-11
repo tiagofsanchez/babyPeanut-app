06698e7a7d391479086e51caf9d18aad28a8b66c
tiagofsanchez <tiagofsanchez@users.noreply.github.com>
2019-03-11 22:30:04 +0800

    dashboarding

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -2,58 +2,43 @@ import React from 'react';
 import styled from 'styled-components';
 import DashboardFoodSummary from './DashboardFoodSummary';
 import DashboardOutputSummary from './DashboardOutputSummary';
+import PlaceHolder from './PlaceHolder';
 
 /* ****************** */
 /* Styled Components */
 /* ****************** */
 const DashboardWrapper = styled.div`
-    display: grid;
-    grid-template-columns: 33.33% 33.33% 33.33% ;
-    grid-template-row: 50% 50% ;  
-    height: 100%;
-    padding: 50px;  
-    grid-gap: 20px;
-    justify-content: space-around;
-    grid-template-areas: 
-    "Graph1 Graph2 Graph3"
-    "Graph4 Graph5 Graph6"
+    display: flex;
 `;
 
 const Graph1Wrapper = styled.div`
-    grid-area: Graph1
+    flex-basis: 200px; 
+    height:50%;
     background: rgba(255, 255, 255, 0.2);
     box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
 `;
 
 const Graph2Wrapper = styled.div`
-    grid-area: Graph2
+    flex-basis: 800px; 
+    height:50%;
     background: rgba(255, 255, 255, 0.2);
     box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
 `;
 
-const Graph3Wrapper = styled.div`
-    grid-area: Graph3
-    background: rgba(255, 255, 255, 0.2);
-    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-`;
+
 
 const Graph4Wrapper = styled.div`
-    grid-area: Graph4
+    flex-basis: 200px; 
     background: rgba(255, 255, 255, 0.2);
     box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
 `;
 
 const Graph5Wrapper = styled.div`
-    grid-area: Graph5
+    flex-basis: 800px; 
     background: rgba(255, 255, 255, 0.2);
     box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
 `;
 
-const Graph6Wrapper = styled.div`
-    grid-area: Graph6
-    background: rgba(255, 255, 255, 0.2);
-    box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
-`;
 
 
 /* ****************** */
@@ -65,26 +50,24 @@ class Dashboard extends React.Component {
         const { food, output } = this.props;
 
         return (
-            <DashboardWrapper>
-                <Graph1Wrapper>
-                    <DashboardFoodSummary food={food} />
-                </Graph1Wrapper>
-                <Graph2Wrapper>
-                    Heeeee
-                </Graph2Wrapper>
-                <Graph3Wrapper>
-                
-                </Graph3Wrapper>
+            <div>
+                <DashboardWrapper>
+                    <Graph1Wrapper>
+                        <DashboardFoodSummary food={food} />
+                    </Graph1Wrapper>
+                    <Graph2Wrapper>
+                        <PlaceHolder />
+                    </Graph2Wrapper>
+                </DashboardWrapper>
+                <DashboardWrapper>
                 <Graph4Wrapper>
-                    <DashboardOutputSummary output={output}/>
+                    <DashboardOutputSummary output={output} />
                 </Graph4Wrapper>
                 <Graph5Wrapper>
-                    Heeeee
+                    <PlaceHolder />
                 </Graph5Wrapper>
-                <Graph6Wrapper>
-                    Heeeee
-                </Graph6Wrapper>
-            </DashboardWrapper>
+                </DashboardWrapper>
+            </div>
         )
     }
 }
