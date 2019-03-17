c296b74d8f6b7b6e186ffc7f605d4f3f2d25d989
Tiago Formosinho Sanchez <tiagofsanchez@gmail.com>
2019-03-17 22:32:18 +0800

    graph is working

--- src/Components/FoodChart.js
+++ src/Components/FoodChart.js
@@ -38,35 +38,15 @@ class FoodChart extends React.Component {
         const { food } = this.props;
 
         const groupByFood = this.groupBy(food, "datetime");
-        const newFood = Object.keys(groupByFood).map(item => { 
-            return {
-                [item]:{
-                    data: groupByFood[item].data,
-                    quantity: groupByFood[item].quantity,
-                    duration: groupByFood[item].duration,
-            }
-            }
+        const data = Object.keys(groupByFood).map(item => {
+            const date = item.substring(6, 10) + -+ item.substring(3, 5) + -+ item.substring(0, 2);
+            const newDate = moment(date).format('Do MMM');
+            return ({ 
+                name: newDate, 
+                uv: groupByFood[item].quantity, 
+                pv: groupByFood[item].duration 
+            })
         })
-
-        console.log(newFood);
-
-        /* const data = [
-            { name: 'Page A', uv: 4000, pv: 2400, amt: 2400 },
-            { name: 'Page B', uv: 3000, pv: 1398, amt: 2210 },
-            { name: 'Page C', uv: 2000, pv: 9800, amt: 2290 },
-            { name: 'Page D', uv: 2780, pv: 3908, amt: 2000 },
-            { name: 'Page E', uv: 1890, pv: 4800, amt: 2181 },
-            { name: 'Page F', uv: 2390, pv: 3800, amt: 2500 },
-            { name: 'Page G', uv: 3490, pv: 4300, amt: 2100 },
-        ];   */
-        
-         const data = newFood.map(item => {
-            /* const date = item.substring(6, 10) + -+ item.substring(3, 5) + -+ item.substring(0, 2);
-            const newDate = moment(date).format('Do MMM'); */
-            return ({ name: item , uv: '', pv: ''})
-        })  
-                
-        console.log(data);
 
         return (
             <GraphWrapper>
